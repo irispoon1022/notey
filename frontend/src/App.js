21a92e0359e01a4f1b59d1b1050f0e11d9622eb1
irispoon1022 <timerainy2@gmail.com>
2019-09-30 19:36:40 +0800

    display upnote and downnote

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoading: true,
-        data: []
-      };
-    }
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: []
+    };
+  }
 
-    componentDidMount() {
-      fetch("http://localhost:8080/api/v1/notes")
-        .then(response => response.json())
-        .then(body => {
-          this.setState({ 
-            data: body,
-          isLoading:false, });
+  componentDidMount() {
+    fetch("http://localhost:8080/api/v1/notes")
+      .then(response => response.json())
+      .then(body => {
+        this.setState({
+          data: body,
+          isLoading: false
         });
-    }
-    render() {
-      const { isLoading, data } = this.state;
-      return (<React.Fragment>
-        <h1>Random User</h1>
+      });
+  }
+  render() {
+    const { isLoading, data } = this.state;
+    return (
+      <React.Fragment>
         {!isLoading ? (
           data.map(datum => {
-            const { id,content } = datum;
+            const { id, content, upnote, downnote } = datum;
+
             return (
               <div key={id}>
-                <p>Name: {content}</p>
+                {(Array.isArray(upnote) && upnote.length) > 0 &&
+                  upnote.map(upnotea => <p>upnote:{upnotea.content}</p>)}
+                <p>{id}</p>
+                <p>{content}</p>
+                {(Array.isArray(downnote) && downnote.length) > 0 &&
+                  downnote.map(downnotea => <p>downnote:{downnotea.content}</p>)}
                 <hr />
               </div>
             );
           })
-        // If there is a delay in data, let's let the user know it's loading
         ) : (
+          // If there is a delay in data, let's let the user know it's loading
           <h3>Loading...</h3>
         )}
       </React.Fragment>
-      );
-    }
-
+    );
+  }
 }
 
 export default App;
