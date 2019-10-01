56b0e1345aabc07a3216e29bed8311c5b0247532
irispoon1022 <timerainy2@gmail.com>
2019-10-01 17:53:02 +0800

    fix CORS problem

--- backend/src/main/java/com/notey/notey/controllers/NotesController.java
+++ backend/src/main/java/com/notey/notey/controllers/NotesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 @RequestMapping("/api/v1/notes")
 public class NotesController {
