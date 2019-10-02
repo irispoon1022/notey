e484521a0a32c9e46d5e3496ddf029c416bd7853
irispoon1022 <timerainy2@gmail.com>
2019-10-02 21:23:35 +0800

    style change to app bar text

--- frontend/src/SearchAppBar.js
+++ frontend/src/SearchAppBar.js
@@ -76,7 +76,7 @@ export default function SearchAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
+          <Typography className={classes.title} variant="h6" style={{color:"#ffffff"}} noWrap>
             Wisdombite
           </Typography>
           <div className={classes.search}>
