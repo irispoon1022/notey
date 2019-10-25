6e687b48585602e603b83b8d3099124f92ed4d1c
irispoon1022 <timerainy2@gmail.com>
2019-10-25 22:09:02 +0800

    enable the upnote dropdown to post data to backend

--- frontend/src/components/Note/Note.js
+++ frontend/src/components/Note/Note.js
@@ -2,106 +2,194 @@ import React, { Component } from "react";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
 import { red } from "@material-ui/core/colors";
-import './Note.css'
-import ReactMarkdown from 'react-markdown';
-import CreatableSelect from 'react-select/creatable';
+import "./Note.css";
+import ReactMarkdown from "react-markdown";
+import CreatableSelect from "react-select/creatable";
+import Select from "react-select";
+import Button from "@material-ui/core/Button";
 
 // generage select dropdown option list dynamically
 function Options({ options }) {
   return options.map(option => (
-    <option key={option.id} content={option.content} label={option.content} value={option.id}>
+    <option
+      key={option.id}
+      content={option.content}
+      label={option.content}
+      value={option.id}
+    >
       {option.content}
     </option>
   ));
 }
 
 class Note extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            rule:props.rule,
-            value:props.upnote
+  constructor(props) {
+    super(props);
+    this.state = {
+      rule: props.rule,
+      value: props.upnote
+    };
+    this.handleCLick = this.handleClick.bind(this);
+  }
+
+  handleMarkRule = (
+    id,
+    author,
+    content,
+    rule,
+    date,
+    title,
+    web,
+    upnote,
+    downnote
+  ) => {
+    this.setState(
+      prevState => ({
+        rule: !prevState.rule
+      }),
+      () => {
+        const a = {
+          id: id,
+          author: author,
+          content: content,
+          rule: this.state.rule,
+          date: date,
+          title: title,
+          web: web,
+          upnote: upnote,
+          downnote: downnote
         };
-        this.handleSelect=this.handleSelect.bind(this);
-    }
+        axios.put(`http://localhost:8080/api/v1/notes/${id}`, a);
+      }
+    );
+  };
 
-    handleMarkRule = (id, author, content, rule, date, title, web, upnote, downnote) => {
-        this.setState((prevState => ({
-            rule: !prevState.rule
-          })),() => {
-              const a={
-                  id: id,
-                  author:author,
-                  content:content,
-                  rule:this.state.rule,
-                  date:date,
-                  title:title,
-                  web:web,
-                  upnote:upnote,
-                  downnote:downnote
-              }
-              axios.put(`http://localhost:8080/api/v1/notes/${id}`, a)
-          }
+  // handleSelect = id => event => {
+  //   // this.setState({value: event.target.value},()=>{console.log(this.state.value,id)})
+  //   this.setState({ value: event.target.value }, () => {
+  //     axios.post(
+  //       `http://localhost:8080/api/v1/notes/${id}/upnote/${this.state.value}`
+  //     );
+  //   });
+  // };
 
-        )
-    }
-
-   handleSelect = (id) => (event) => {
-      // this.setState({value: event.target.value},()=>{console.log(this.state.value,id)})
-      this.setState({value: event.target.value},()=>{axios.post(`http://localhost:8080/api/v1/notes/${id}/upnote/${this.state.value}`)})
-    }
-
-    handleDeleteRelationship = (id, upnoteid) => {
-      // console.log({id, upnoteid})
-      axios.delete(`http://localhost:8080/api/v1/notes/${id}/upnote/${upnoteid}`).then(res => {
+  handleDeleteRelationship = (id, upnoteid) => {
+    // console.log({id, upnoteid})
+    axios
+      .delete(`http://localhost:8080/api/v1/notes/${id}/upnote/${upnoteid}`)
+      .then(res => {
         console.log(res);
         console.log(res.data);
       });
-    };
+  };
 
-    handleChange = (newValue: any, actionMeta: any) => {
-      console.group('Value Changed');
-      console.log(newValue);
-      console.log(`action: ${actionMeta.action}`);
-      console.groupEnd();
-    };
+  handleChange = (newValue: any, actionMeta: any) => {
+    console.group("Value Changed");
+    console.log(newValue);
+    console.log(`action: ${actionMeta.action}`);
+    console.groupEnd();
+    this.setState({
+      value: newValue
+    });
+  };
+  handleClick = id => {
+    this.state.value.forEach(valueElement => {
+      console.log(id, valueElement.value);
+      axios.post(
+        `http://localhost:8080/api/v1/notes/${id}/upnote/${valueElement.value}`
+      );
+    });
+  };
 
   render() {
-    const { id, author, content, rule, date, title, web, upnote, downnote, ruleIsLoading, rulenotes, handleDelete } = this.props;
+    const {
+      id,
+      author,
+      content,
+      rule,
+      date,
+      title,
+      web,
+      upnote,
+      downnote,
+      ruleIsLoading,
+      rulenotes,
+      handleDelete,
+      value
+    } = this.props;
     return (
       <Paper style={{ padding: 30, margin: 20 }}>
-        <select style={{ width:"80%"}} name="animal" value={this.state.value} onChange={this.handleSelect(id)}>
+        {/* <select
+          style={{ width: "80%" }}
+          name="animal"
+          value={this.state.value}
+          onChange={this.handleSelect(id)}
+        >
           <Options options={!ruleIsLoading ? rulenotes : []} />
-        </select> 
-        <CreatableSelect
-        isMulti
-        onChange={this.handleChange}
-        // options={colourOptions}
-        options= {!ruleIsLoading ? rulenotes.map (rulenote =>{
-          var emptyObj={};
-          emptyObj.value = rulenote.id;
-          emptyObj.label = rulenote.title;
-          return emptyObj;
-        }) : []}
-      />
-        
-        <ReactMarkdown escapeHtml= {false} source={content}/>
+        </select> */}
+        <div style={{"padding-bottom":"20px"}}>
+          <div style={{ width: "90%", float: "left" }}>
+            <CreatableSelect
+              isMulti
+              defaultValue={{ value: 1, label: "hi" }}
+              onChange={this.handleChange}
+              value={this.state.value}
+              options={
+                !ruleIsLoading
+                  ? rulenotes.map(rulenote => {
+                      var emptyObj = {};
+                      emptyObj.value = rulenote.id;
+                      emptyObj.label = rulenote.title;
+                      return emptyObj;
+                    })
+                  : []
+              }
+            />
+          </div>
+          <Button variant="outlined" onClick={this.handleClick.bind(this, id)} style={{ "margin-left": "10px"}}>
+            Submit
+          </Button>
+        </div>
+        <ReactMarkdown escapeHtml={false} source={content} />
         {(Array.isArray(upnote) && upnote.length) > 0 &&
           upnote.map(upnotea => {
-            return(
+            return (
               <div>
                 <p>Can be explained by: {upnotea.content}</p>
-                <button onClick={this.handleDeleteRelationship.bind(this, id, upnotea.id)}>Delete relationship</button>
+
+                <Button
+                  variant="outlined"
+                  onClick={this.handleDeleteRelationship.bind(
+                    this,
+                    id,
+                    upnotea.id
+                  )}
+                >
+                  Delete
+                </Button>
               </div>
-        
-          )})
-        }
+            );
+          })}
         {(Array.isArray(downnote) && downnote.length) > 0 &&
-          downnote.map(downnotea => <p>Related example: {downnotea.content}</p>)}
+          downnote.map(downnotea => (
+            <p>Related example: {downnotea.content}</p>
+          ))}
         <button onClick={handleDelete.bind(this, id)}>Delete</button>
-        <button 
-        className={this.state.rule?"buttonred":"buttongrey"}
-        onClick={this.handleMarkRule.bind(this, id, author, content, rule, date, title, web, upnote, downnote)}>
+        <button
+          className={this.state.rule ? "buttonred" : "buttongrey"}
+          onClick={this.handleMarkRule.bind(
+            this,
+            id,
+            author,
+            content,
+            rule,
+            date,
+            title,
+            web,
+            upnote,
+            downnote
+          )}
+        >
           Mark as Rule
         </button>
       </Paper>
