922f381cca65b4ceb2137753f8b368cb8fa1193e
irispoon1022 <timerainy2@gmail.com>
2019-11-29 17:14:41 +0800

    remove bind from "app.js"

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,18 +5,18 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
 import Note from "./Note/Note";
-import Chip from '@material-ui/core/Chip';
-import LazyLoad from 'react-lazyload';
+import Chip from "@material-ui/core/Chip";
+import LazyLoad from "react-lazyload";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(0.5),
-    },
-  },
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5)
+    }
+  }
 }));
 
 class App extends Component {
@@ -27,8 +27,8 @@ class App extends Component {
       data: [],
       ruleIsLoading: true,
       rulenotes: [],
-      tagIsLoading:true,
-      tags:[],
+      tagIsLoading: true,
+      tags: []
     };
   }
 
@@ -47,89 +47,105 @@ class App extends Component {
       })
       .catch(error => console.log(error));
 
-      axios
+    axios
       .get("http://localhost:8080/api/v1/tags")
       .then(response => {
         this.setState({ tags: response.data, tagIsLoading: false });
       })
       .catch(error => console.log(error));
-
   }
+
   handleDelete = id => {
-    
-    const updatedData = 
-    this.state.data.filter(function isNotId(datum){
-      return (
-        datum.id !==id
-      )
-    })
+    const updatedData = this.state.data.filter(function isNotId(datum) {
+      return datum.id !== id;
+    });
 
     this.setState({
-      data:updatedData
-    })
+      data: updatedData
+    });
 
-    axios.delete(`http://localhost:8080/api/v1/notes/${id}`).then(
-
-    );
+    axios.delete(`http://localhost:8080/api/v1/notes/${id}`).then();
   };
   handleMarkRule = id => {};
 
-  handleClick = (tagId,tags) => {
-    console.log(tagId,tags)
+  handleClick = (tagId, tags) => {
+    console.log(tagId, tags);
 
-      function a (tag) {
-        return (
-          tag.id === tagId
-        )
-      }
-      let noteIdList = tags.filter(a)[0].note.map(item=>item.id)
-      console.log(noteIdList)
-      const updatedData = 
-    this.state.data.filter(function matchId(datum){
-      return (
-        noteIdList.includes(datum.id)
-      )
-    })
+    function a(tag) {
+      return tag.id === tagId;
+    }
+    let noteIdList = tags.filter(a)[0].note.map(item => item.id);
+    console.log(noteIdList);
+    const updatedData = this.state.data.filter(function matchId(datum) {
+      return noteIdList.includes(datum.id);
+    });
     this.setState({
-      data:updatedData
-    })
-  }
-s
-  
+      data: updatedData
+    });
+  };
+
 
   render() {
-    const { isLoading, data, rulenotes, ruleIsLoading,tags,tagIsLoading } = this.state;
-    const array=[1,2];
+    const {
+      isLoading,
+      data,
+      rulenotes,
+      ruleIsLoading,
+      tags,
+      tagIsLoading
+    } = this.state;
+    const array = [1, 2];
     return (
       <React.Fragment>
         <SearchAppBar />
-        <div style={{padding:"16px" ,backgroundColor:"#F9FCFF"}}>
-    {<span>Tags:</span>}{!tagIsLoading ? (tags.map (tag => <Chip style={{margin:"8px"}}label={tag.name} onClick={this.handleClick.bind(this,tag.id,tags)}></Chip>)) : (<p>Tag is loading</p>)}
-        
-        {data.map(datum => {
+        <div style={{ padding: "16px", backgroundColor: "#F9FCFF" }}>
+          {<span>Tags:</span>}
+          {!tagIsLoading ? (
+            tags.map(tag => (
+              <Chip
+                style={{ margin: "8px" }}
+                label={tag.name}
+                onClick={() => {this.handleClick(tag.id, tags)}}
+              ></Chip>
+            ))
+          ) : (
+            <p>Tag is loading</p>
+          )}
 
-            const { id, author, content, rule, date, title, web, book, upnote, downnote, } = datum;
+          {data.map(datum => {
+            const {
+              id,
+              author,
+              content,
+              rule,
+              date,
+              title,
+              web,
+              book,
+              upnote,
+              downnote
+            } = datum;
 
             return (
               <LazyLoad key={id} placeholder={<p>loading</p>}>
-              <div key={id}>
-                <Note
-                  id={id}
-                  author={author}
-                  content={content}
-                  rule={rule}
-                  date={date}
-                  title={title}
-                  web={web}
-                  book={book}
-                  upnote={upnote}
-                  downnote={downnote}
-                  ruleIsLoading={ruleIsLoading}
-                  rulenotes={rulenotes}
-                  onRefresh={this.handleRefreshCard}
-                  handleDelete={this.handleDelete}
-                />
-              </div>
+                <div key={id}>
+                  <Note
+                    id={id}
+                    author={author}
+                    content={content}
+                    rule={rule}
+                    date={date}
+                    title={title}
+                    web={web}
+                    book={book}
+                    upnote={upnote}
+                    downnote={downnote}
+                    ruleIsLoading={ruleIsLoading}
+                    rulenotes={rulenotes}
+                    onRefresh={this.handleRefreshCard}
+                    handleDelete={this.handleDelete}
+                  />
+                </div>
               </LazyLoad>
             );
           })}
